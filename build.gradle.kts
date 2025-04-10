8de31096ce703769b509847d3fdfd7df70ebada5
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2025-04-09 21:11:40 -0400

    gradle = "8.13" kotlin = "2.1.20" agp = "8.9.1" bcv = "0.17.0" dokka = "2.0.0" compose = "1.7.3" ktor = "3.1.0" kover = "0.9.1"

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
 
 allprojects {
     group = "app.lexilabs.basic"
-    version = "0.2.6-beta01"
+    version = "0.2.5"
 
     apply(plugin = "org.jetbrains.dokka")
     apply(plugin = "maven-publish")
