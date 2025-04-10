c146d91ca9872124442f79cdd4af7ab3ccc9f4db
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2025-04-09 21:02:51 -0400

    init

---
+++ basic-images/build.gradle.kts
@@ -0,0 +1,100 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    alias(libs.plugins.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlinx.binary.compatibility.validator)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kover)
+    `maven-publish`
+    signing
+}
+
+kotlin {
+
+    // FORCES CHECK OF PUBLIC API DECLARATIONS
+    // DON'T FORGET TO RUN `./gradlew apiDump`
+    explicitApi()
+
+    jvm()
+
+    listOf(
+        iosX64(), // mobile
+        iosArm64(), // mobile
+        iosSimulatorArm64(), // mobile
+        macosX64(), // desktop
+        macosArm64(), // desktop
+    ).forEach {
+        it.binaries.framework {
+            baseName = "basic-images"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            compileOnly(libs.lexilabs.basic.logging)
+            api(libs.lexilabs.basic.logging)
+            compileOnly(libs.compose.foundation)
+            api(libs.compose.foundation)
+            implementation(libs.compose.material3)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.resources)
+        }
+        androidMain.dependencies {
+            implementation(libs.ktor.client.okhttp)
+            implementation(libs.ktor.client.android)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+            implementation(libs.ktor.client.ios)
+        }
+        macosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+            implementation(libs.ktor.client.ios)
+        }
+    }
+
+    //https://kotlinlang.org/docs/native-objc-interop.html#export-of-kdoc-comments-to-generated-objective-c-headers
+    targets.withType<KotlinNativeTarget> {
+        compilations["main"].compileTaskProvider.configure{
+            compilerOptions {
+                freeCompilerArgs.add("-Xexport-kdoc")
+            }
+        }
+    }
+
+    compilerOptions {
+        freeCompilerArgs.add("-Xexpect-actual-classes")
+    }
+
+    // Android JVM target target options
+    androidTarget {
+        publishLibraryVariants("release", "debug")
+        compilations.all{
+            compileTaskProvider.configure{
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_17)
+                }
+            }
+        }
+    }
+}
+
+android {
+    namespace = "app.lexilabs.basic.images"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    buildFeatures{
+        compose = true
+    }
+}
